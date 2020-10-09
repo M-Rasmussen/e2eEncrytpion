ef691ea054b32ced78218310a5c827162b3b177d
EC2 Default User <ec2-user@ip-172-31-39-121.ec2.internal>
2020-10-09 15:43:34 +0000

    Creating Git repository to hold information for the project2, made .gitignore and copyed all of the code form an inclass lecture to get the ball rolling.

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+import flask
+import flask_socketio
+
+app = flask.Flask(__name__)
+socketio = flask_socketio.SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+
+@app.route('/')
+def hello():
+    return flask.render_template('index.html')
+
+@socketio.on('connect')
+def on_connect():
+    print('Someone connected!')
+    socketio.emit('connected', {
+        'test': 'Connected'
+    })
+
+@socketio.on('disconnect')
+def on_disconnect():
+    print ('Someone disconnected!')
+
+@socketio.on('new number')
+def on_new_number(data):
+    print("Got an event for new number with data:", data)
+    rand_number = data['number']
+    socketio.emit('number received', {
+        'number': rand_number
+    })
+
+if __name__ == '__main__': 
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
